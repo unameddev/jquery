23234a4321a1950f3916260289b5652a7acf1397
Pedro Roque <dotjp1@gmail.com>
2023-12-04 17:04:11 -0300

    webstore updates

--- js/custom.js
+++ js/custom.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
     // $('.featured-item:first h4').fadeOut()
     //  $('.featured-item:first h4').css('color', '#f00')
         
-        $('.featured-item h4').dblclick( function(){
+    $('.featured-item h4').dblclick( function(){
 
         $(this).css({
             'color': '#f00',
@@ -36,18 +36,73 @@ $(document).ready(function(){
             'font-weight': '100',
         });
 
-        });
+    });
 
         /*
         * Manipulação de eventos
         */
-        $('.featured-item a').on('click', function(event){
+    $('.featured-item a').on('click', function(event){
 
         event.preventDefault();
 
         alert('Produto esgotado');
 
+    })
+
+    /* 
+     * Callback
+     * entendendo ações que começam ao termino de outra
+    */
+    $('.featured-item:nth(1)')
+        .hide(2000, function(){
+            // este é o callback
+            console.log($(this).find('h4').text() + ' esgotado')
+        })
+        .show(2000, function(){
+            console.log($(this).find('h4').text() + ' em estoque')
         })
 
-        
-})
+    /*
+     * Animações
+    */
+   const duracao = 1000 //equivale a 1 segundo
+   $('.featured-item:nth(0)')
+        .hide(duracao)
+        .show(duracao)
+        .fadeOut(duracao)
+        .fadeIn(duracao)
+        .toggle(duracao)
+        .toggle(duracao)
+
+    $('#form-submit').on('click', function(e){
+
+        e.preventDefault()
+
+       if($('#email').val() != ''){
+
+        $('#email').animate({
+            'opacity': 'toggle',
+            'top':'-50',
+
+        }, duracao, function(){
+            console.log($(this).val())
+        });
+
+       } 
+
+    });
+
+    /*
+    * Ouvinte de eventos .nav-modal-open
+    */
+    $('.nav-modal-open').on('click', function(e){
+        e.preventDefault();
+
+        let elem = $(this).attr('rel')
+
+        $('.modal-body').html($('#'+elem).html())
+
+    });
+
+
+});
